b14fd9040a066af5f7d01b9568c583bfd90df944
Vitvara Varavithya <gridxlgm@gmail.com>
2021-09-20 11:46:49 +0700

    Add parameterized_test.py

--- parameterized_test.py
+++ parameterized_test.py
@@ -3,20 +3,30 @@ from triangle import is_triangle
 
 class TriangleTest(unittest.TestCase):
 
-    def test_valid_triangle(self):
-        self.assertTrue( is_triangle(1, 1, 1) )
-        self.assertTrue( is_triangle(3, 4, 5) )
-        self.assertTrue( is_triangle(3, 4, 6) )
-        self.assertTrue( is_triangle(8, 10, 12) )
-        self.assertTrue( is_triangle(100, 101, 200) )
-        self.assertTrue( is_triangle(0.9, 1.0, 1.1) )
+    valid_triangles = [
+            (1, 1, 1),
+            (3, 4, 5),
+            (3, 4, 6),  
+            ]
 
+    def test_valid_triangle(self):
+        for a,b,c in self.valid_triangles:
+            with self.subTest():
+                msg = f"side lengths ({a},{b},{c})"
+                self.assertTrue( is_triangle(a, b, c), msg)
+
+    not_valid_triangles = [
+            (21, 10, 10),
+            (2, 1, 1),
+            (6, 10, 4),
+            (6, 20, 4),
+            ( 1, 2, 4) 
+            ]
     def test_not_triangle(self):
-        self.assertFalse( is_triangle(21, 10, 10) )
-        self.assertFalse( is_triangle(2, 1, 1) )   # borderline case
-        self.assertFalse( is_triangle(6, 10, 4) )  # borderline case
-        self.assertFalse( is_triangle(6, 20, 4) )
-    
+        for a,b,c in self.not_valid_triangles:
+            with self.subTest():
+                self.assertFalse( is_triangle(a, b, c))
+
     def test_invalid_argument_raises_exception(self):
         """any non-positive argument should raise ValueError"""
         with self.assertRaises(ValueError):
@@ -30,6 +40,4 @@ class TriangleTest(unittest.TestCase):
 
         with self.assertRaises(ValueError):
             b1 = is_triangle( -1, -1, -1)
-
-        self.assertFalse(is_triangle( 1, 2, 4))
         
